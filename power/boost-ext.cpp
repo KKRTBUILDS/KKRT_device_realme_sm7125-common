214673bb04d216ea24999976c776474e38386a2b
Nauval Rizky <enuma.alrizky@gmail.com>
2022-03-25 01:18:43 +0300

    sm7125-common: power: Add custom boost extension for scroll handling
    Currently, we're not using CAF's BoostFramework due to having a pure
    AOSP based source, thus qcom perf hinting are not correctly applied.
    This extension will handle the vertical fling through device-specific
    boost override, utilizing existing libqti-perfd hinting.
    So any further configuration should be happens inside vertical scroll
    config inside perfboostsconfig.xml
    
    Reference: vendor/qcom/opensource/power/power-6150.c w/ reduced
    max interactive timeout
    
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- power/boost-ext.cpp
+++ power/boost-ext.cpp
@@ -43,7 +43,11 @@ extern "C" {
 #include "utils.h"
 
 const int kMinInteractiveDuration = 100;  /* ms */
+<<<<<<< HEAD
 const int kMaxInteractiveDuration = 5000; /* ms */
+=======
+const int kMaxInteractiveDuration = 2500; /* ms */
+>>>>>>> b26e2cb... sm7125-common: power: Add custom boost extension for scroll handling
 
 static int process_interaction_hint(void* data) {
     static struct timespec s_previous_boost_timespec;
